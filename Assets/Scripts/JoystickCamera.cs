78ba541e4164d57c558d7d7eb332749750a5eeb0
Seb <S.chevallier.pro@gmail.com>
2022-03-09 20:10:44 +0100

    PlayerMovement, jump et roll

---
+++ Assets/Scripts/JoystickCamera.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoystickCamera : MonoBehaviour
+{
+    [SerializeField]
+    float sensitivityX = 8f;
+    [SerializeField]
+    float sensitivityY = 0.5f;
+    float xRotation = 0f;
+
+
+    [SerializeField] Transform playerCamera;
+    [SerializeField] float xClamp = 85f;
+
+    private void FixedUpdate()
+    {      
+        transform.Rotate(Vector3.up, Input.GetAxis("JoystickHorizontal") * sensitivityX * Time.deltaTime);
+        xRotation += Input.GetAxis("JoystickVertical");
+        xRotation = Mathf.Clamp(xRotation, -xClamp, xClamp);
+        Vector3 targetRotation = transform.eulerAngles;
+        targetRotation.x = xRotation;
+        targetRotation.z = 0;
+        playerCamera.eulerAngles = targetRotation;
+
+
+    }
+
+
+}
