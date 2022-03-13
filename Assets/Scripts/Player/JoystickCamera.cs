64f4577f142f62d5086a8a6cab771d3b4e948cd5
Seb <S.chevallier.pro@gmail.com>
2022-03-13 20:51:11 +0100

    TestTerrainMat

--- Assets/Scripts/Player/JoystickCamera.cs
+++ Assets/Scripts/Player/JoystickCamera.cs
@@ -17,14 +17,17 @@ public class JoystickCamera : MonoBehaviour
 
 
     private void FixedUpdate()
-    {      
-        transform.Rotate(Vector3.up, Input.GetAxis("JoystickHorizontal") * sensitivityX * Time.deltaTime);
-        xRotation += Input.GetAxis("JoystickVertical");
-        xRotation = Mathf.Clamp(xRotation, minXClamp, xClamp);
-        Vector3 targetRotation = transform.eulerAngles;
-        targetRotation.x = xRotation;
-        targetRotation.z = 0;
-        playerCamera.eulerAngles = targetRotation;
+    {
+        if (Input.GetAxis("JoystickHorizontal") > 0.01f  || Input.GetAxis("JoystickVertical") > 0.01f || Input.GetAxis("JoystickHorizontal") < -0.01f || Input.GetAxis("JoystickVertical") < -0.01f)
+        {
+            transform.Rotate(Vector3.up, Input.GetAxis("JoystickHorizontal") * sensitivityX * Time.deltaTime);
+            xRotation += Input.GetAxis("JoystickVertical");
+            xRotation = Mathf.Clamp(xRotation, minXClamp, xClamp);
+            Vector3 targetRotation = transform.eulerAngles;
+            targetRotation.x = xRotation;
+            targetRotation.z = 0;
+            playerCamera.eulerAngles = targetRotation;
+        } 
 
 
     }
